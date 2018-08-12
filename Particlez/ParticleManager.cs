b37898d5e7072a8e42f2615fbf128f01d8727d90
Flowaria <wnsghgj1234@naver.com>
2018-08-12 22:31:21 +0900

    particle

---
+++ Particlez/ParticleManager.cs
@@ -0,0 +1,121 @@
+﻿using EasyRequest;
+using Lanotalium.Chart;
+using Lanotalium.Plugin;
+using Lanotalium.Plugin.Events.Chart;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Flowaria.Lanotalium.Plugin
+{
+
+    public class ParticleManagerPreferences
+    {
+        [Name("Use Particle effect")]
+        public bool UseClickParticle = true;
+
+        [Name("Use Sound Effect for key pressed event")]
+        public bool UseRailParticle = true;
+
+        [Name("Use Sound Effect for key pressed event")]
+        public bool UseFlickParticle = true;
+
+        [Name("Show Combo count when judge text shows")]
+        public bool UseComboCount = true;
+    }
+
+    public class GameObjectBool
+    {
+        public GameObject obj = null;
+        public bool actived = false;
+
+        public GameObjectBool(GameObject obj, bool actived)
+        {
+            this.obj = obj;
+            this.actived = actived;
+        }
+    }
+
+    public class ParticleManager : MonoBehaviour
+    {
+        public LanotaliumContext context;
+        public ParticleManagerPreferences pref;
+
+        public GameObject PrefabHoldSparkle;
+
+        
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            UpdateTapNote();
+            UpdateHoldNote();
+        }
+
+        void UpdateTapNote()
+        {
+            foreach (var note in context.TunerManager.TapNoteManager.TapNote)
+            {
+                
+            }
+        }
+
+        void OnTapNoteClicked(LanotaTapNote note)
+        {
+
+        }
+
+        void OnHoldNoteClicked(LanotaHoldNote note)
+        {
+
+        }
+
+        public Dictionary<LanotaHoldNote, GameObjectBool> pHoldSparkle = new Dictionary<LanotaHoldNote, GameObjectBool>();
+        void UpdateHoldNote()
+        {
+            foreach (var note in context.TunerManager.HoldNoteManager.HoldNote)
+            {
+                if (!note.shouldUpdate)
+                    continue;
+
+                float time = context.TunerManager.ChartTime;
+                bool playing = context.TunerManager.MediaPlayerManager.IsPlaying;
+
+
+                if (note.shouldUpdate && note.StartEffectPlayed)
+                {
+                    if(!pHoldSparkle.ContainsKey(note))
+                    {
+                        var particle = GameObject.Instantiate(PrefabHoldSparkle, note.HoldNoteGameObject.transform.Find("Note"), false);
+                        pHoldSparkle.Add(note, new GameObjectBool(particle, true));
+                    }
+                }
+                else if (note.shouldUpdate && note.EndEffectPlayed)
+                {
+                    if(pHoldSparkle.ContainsKey(note))
+                    {
+                        GameObject.Destroy(pHoldSparkle[note].obj);
+                        pHoldSparkle.Remove(note);
+                    }
+                }
+                else if(!note.shouldUpdate)
+                {
+                    if (pHoldSparkle.ContainsKey(note))
+                    {
+                        GameObject.Destroy(pHoldSparkle[note].obj);
+                        pHoldSparkle.Remove(note);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Particlez/ParticleZ.cs
@@ -0,0 +1,99 @@
+﻿using EasyRequest;
+using Lanotalium.Plugin;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Flowaria.Lanotalium.Plugin
+{
+    public class ParticleZCore : ILanotaliumPlugin
+    {
+        public string Name(Language lang)
+        {
+            return "ParticleZ - Switch";
+        }
+
+        public string Description(Language lang)
+        {
+            return "Turn on it god dammit!";
+        }
+
+        public IEnumerator Process(LanotaliumContext context)
+        {
+            var manager = Manager.Create(context);
+
+            var Bundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "Assets/lanotaparticle"));
+            if (Bundle != null)
+            {
+                manager.GetComponent<ParticleManager>().PrefabHoldSparkle = Bundle.LoadAsset<GameObject>("Assets/Particle/HoldNoteSparkle.prefab");
+            }
+            yield return null;
+        }
+    }
+    
+    public class ParticleZPreferences : ILanotaliumPlugin
+    {
+        public string Name(Language lang)
+        {
+            return "ParticleZ - Preferences";
+        }
+
+        public string Description(Language lang)
+        {
+            return "Setting the ParticleZ";
+        }
+
+        public IEnumerator Process(LanotaliumContext context)
+        {
+            
+            Request<ParticleManagerPreferences> r = new Request<ParticleManagerPreferences>();
+            yield return context.UserRequest.Request(r, "Preferences Window");
+            if (r.Succeed)
+            {
+                Manager.Setting(r.Object);
+            }
+            yield return null;
+        }
+    }
+
+    public class Manager
+    {
+        public static GameObject Create(LanotaliumContext context)
+        {
+            if(!Exists())
+            {
+                var manager = new GameObject("pParticleManager");
+                if(manager != null)
+                {
+                    manager.AddComponent<ParticleManager>();
+                    manager.GetComponent<ParticleManager>().context = context;
+                    manager.SetActive(true);
+                    return manager;
+                }
+                return null;
+            }
+            else
+            {
+                return GameObject.Find("pParticleManager");
+            }
+        }
+
+        public static void Setting(ParticleManagerPreferences pref)
+        {
+            if (Exists())
+            {
+                GameObject.Find("pParticleManager").GetComponent<ParticleManager>().pref = pref;
+            }
+        }
+
+        public static bool Exists()
+        {
+            return GameObject.Find("pParticleManager") != null;
+        }
+    }
+}
